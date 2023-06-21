c9da59b8566d95d26a0e06390e373f7473a0c1c6
OlatmiSE <mygmail.com>
2023-06-20 21:58:46 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+*_putchar.c
+*@c: The character to print
+*
+Return: On success 1.
+*On erro, -1 is retuned, and errno is set appropriately
+*/
+int_putchar(char c)
+{
+      return (1, &c 1));
+}
